f44641c4a4530dd8b50e77427891f791c7f242ab
TheCityCrafter <eliaskretschmer2008@gmail.com>
2025-05-07 17:28:04 +0200

    reworked request loop + today canceled Lessons

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import asyncio
 from datetime import date, timedelta, datetime
 from time import sleep
 
 import discord
 import webuntis
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 import os
 import json
@@ -14,12 +15,13 @@ CLASS = os.getenv("CLASS")
 
 intents = discord.Intents.all()
 
-activity = discord.Activity(type=discord.ActivityType.custom, name="Hallo")
+activity = discord.Activity(type=discord.ActivityType.custom, state="Nicht aktiviert")
 
 bot = discord.Bot(intents=intents,
                   debug_guilds=[802651359743705139],
-                  status=discord.Status.dnd,
+                  status=discord.Status.online,
                   activity=activity)
+
 
 
 class MyView(discord.ui.View):
@@ -32,6 +34,8 @@ async def on_ready():
     print(f"{bot.user} ist online")
 
 channel = bot.get_channel(1369371980238819359)
+
+# === Daten überprüfung ===
 
 try:
     session = webuntis.Session(
@@ -49,70 +53,103 @@ except Exception as e:
    exit()
 
 
-@bot.command(description="Starte den Bot")
+
+@tasks.loop(seconds=10)
+async def send_request():
+    # === API ===
+    session.logout()
+    session.login()
+    FILEPATH = 'canceledLessons.json'
+    startDate = date.today()
+    endDate = startDate + timedelta(days=30)
+
+    # === Vorherige Einträge laden (mit Fehlerprüfung) ===
+    if os.path.exists(FILEPATH) and os.path.getsize(FILEPATH) > 0:
+        with open(FILEPATH, 'r', encoding='utf-8') as f:
+            try:
+                knownCanceledLessons = json.load(f)
+            except json.JSONDecodeError:
+                print("Warnung: JSON-Datei war ungültig. Sie wird zurückgesetzt.")
+                knownCanceledLessons = []
+    else:
+        knownCanceledLessons = []
+
+    # === Stundenplan abrufen ===
+    timetable = s.timetable(klasse=klasse, start=startDate, end=endDate)
+    ausfaelle = [l for l in timetable if l.code == 'cancelled']
+
+    # === Heutige Ausfälle zählen ===
+
+    todayAusfaelle = [l for l in s.timetable(klasse=klasse, start=startDate, end=startDate) if l.code == 'cancelled']
+    todayAusfaelleCount = 0
+    for lesson in todayAusfaelle:
+        todayAusfaelleCount += 1
+
+    await bot.change_presence(
+        activity=discord.Activity(type=discord.ActivityType.custom, state=f"Heutige Ausfälle: {todayAusfaelleCount}"))
+
+    # === Neue Ausfälle erkennen und speichern ===
+    newCanceledLessons = []
+
+    for lesson in ausfaelle:
+        data = {
+            "datum": lesson.start.date().isoformat(),
+            "start": lesson.start.strftime('%H:%M'),
+            "end": lesson.end.strftime('%H:%M'),
+            "lesson": lesson.subjects[0].name if lesson.subjects else "kein Fach",
+            "teacher": lesson.teachers[0].name if lesson.teachers else "kein Lehrer"
+        }
+
+        if data not in knownCanceledLessons:
+            newCanceledLessons.append(data)
+            knownCanceledLessons.append(data)
+
+    # === JSON-Datei aktualisieren ===
+    with open(FILEPATH, 'w', encoding='utf-8') as f:
+        json.dump(knownCanceledLessons, f, indent=2, ensure_ascii=False)
+
+    # === Ausgabe neuer Ausfälle ===
+    if not newCanceledLessons:
+        print(f"Keine neuen Ausfälle")
+    else:
+        for data in newCanceledLessons:
+            print("Neuer Ausfall: ")
+            print(f"{data['datum']} | {data['start']}–{data['end']} | {data['lesson']} mit {data['teacher']}")
+
+            embed = discord.Embed(title=f":x: {data['lesson']}",
+                                  description=f"**{data['lesson']}** mit {data['teacher']} entfällt!\n> {data['datum']} - {data['start']}–{data['end']}",
+                                  colour=0xff0000,
+                                  timestamp=datetime.now())
+
+            embed.set_author(name="Untis-Aktualisierung")
+
+            embed.set_footer(text="Untis-Bot")
+
+            await bot.get_channel(1369371980238819359).send(embed=embed)
+
+
+
+
+@bot.command(description="Start the Bot")
 @commands.has_permissions(administrator=True)
 async def start(ctx):
     await ctx.respond("Done!", ephemeral=True)
-    while True:
-        # API
-        FILEPATH = 'canceledLessons.json'
-        startDate = date.today()
-        endDate = startDate + timedelta(days=30)
+    send_request.start()
+    print("Ein Loop wurde gestartet")
 
-        # === Vorherige Einträge laden (mit Fehlerprüfung) ===
-        if os.path.exists(FILEPATH) and os.path.getsize(FILEPATH) > 0:
-            with open(FILEPATH, 'r', encoding='utf-8') as f:
-                try:
-                    knownCanceledLessons = json.load(f)
-                except json.JSONDecodeError:
-                    print("Warnung: JSON-Datei war ungültig. Sie wird zurückgesetzt.")
-                    knownCanceledLessons = []
-        else:
-            knownCanceledLessons = []
 
-        # === Stundenplan abrufen ===
-        timetable = s.timetable(klasse=klasse, start=startDate, end=endDate)
-        ausfaelle = [l for l in timetable if l.code == 'cancelled']
 
-        # === Neue Ausfälle erkennen und speichern ===
-        newCanceledLessons = []
+@bot.command(description="Stop the Bot")
+@commands.has_permissions(administrator=True)
+async def stop(ctx):
+    await ctx.respond("Done!", ephemeral=True)
+    send_request.stop()
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.custom, state="Nicht aktiviert"))
+    print("Ein Loop wurde beendet")
 
-        for lesson in ausfaelle:
-            data = {
-                "datum": lesson.start.date().isoformat(),
-                "start": lesson.start.strftime('%H:%M'),
-                "end": lesson.end.strftime('%H:%M'),
-                "lesson": lesson.subjects[0].name if lesson.subjects else "kein Fach",
-                "teacher": lesson.teachers[0].name if lesson.teachers else "kein Lehrer"
-            }
 
-            if data not in knownCanceledLessons:
-                newCanceledLessons.append(data)
-                knownCanceledLessons.append(data)
 
-        # === JSON-Datei aktualisieren ===
-        with open(FILEPATH, 'w', encoding='utf-8') as f:
-            json.dump(knownCanceledLessons, f, indent=2, ensure_ascii=False)
 
-        # === Ausgabe neuer Ausfälle ===
-        if not newCanceledLessons:
-            print("Keine neuen Ausfälle")
-        else:
-            for data in newCanceledLessons:
-                print("Neuer Ausfall: ")
-                print(f"{data['datum']} | {data['start']}–{data['end']} | {data['lesson']} mit {data['teacher']}")
-
-                embed = discord.Embed(title=f":x: {data['lesson']}",
-                                      description=f"**{data['lesson']}** mit {data['teacher']} entfällt!\n> {data['datum']} - {data['start']}–{data['end']}",
-                                      colour=0xff0000,
-                                      timestamp=datetime.now())
-
-                embed.set_author(name="Untis-Aktualisierung")
-
-                embed.set_footer(text="Untis-Bot")
-
-                # await bot.get_channel(1369371980238819359).send(embed = embed)
-        sleep(10)
 
 load_dotenv()
 try:
